2e26c8c4cbe44dd7b75775aa15038a7ad5c319f4
James Adcott <l19rvzv3@students.codeinstitute.net>
2025-01-03 13:32:12 +0000

    ready for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-urw#b+70q(jz358%opc78p#&$6h2908chpifz^29lp5c(%6xr!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jamesadcotte-djangoblog-z3dryn1uw21.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-jamesadcotte-djangoblog-z3dryn1uw21.ws.codeinstitute-ide.net', '.herokuapp.com']
 
 
 # Application definition
